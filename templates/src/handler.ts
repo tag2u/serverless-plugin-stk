1c8a0ab57f9c3bce751f2ca79ee1d089d22897b7
H4rry <eriklive2@gmail.com>
2023-11-15 23:33:11 -0300

    new version, added status route for tests

--- templates/src/handler.ts
+++ templates/src/handler.ts
@@ -1,3 +1,12 @@
 export const handler = async (event: unknown, context: unknown) => {
     console.log('Yep, we are up and running! 🚀')
 }
+
+export const status = async (event: unknown, context: unknown) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Hello there!',
+        }),
+    };
+}
